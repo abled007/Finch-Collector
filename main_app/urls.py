b273880babf34c00a16750806a8868f11d66a8d5
abled007 <93559383+abled007@users.noreply.github.com>
2022-04-15 14:35:05 -0700

    Added FinchCreate route

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.Home.as_view(), name="home"),
     path('about/', views.About.as_view(), name="about"),
     path('finch/', views.FinchList.as_view(), name="finch-list"),
+    path('finch/new/', views.Finch_Create.as_view(), name='finch_create'),
+    path('finches/<int:pk>/', views.FinchDetail.as_view(), name="finch_detail"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -3,6 +3,8 @@ from django.views import View
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
 from .models import Finch
+from django.views.generic.edit import CreateView
+from django.views.generic import DetailView
 # Create your views here.
 
 class Home(TemplateView):
@@ -15,10 +17,26 @@ class About(TemplateView):
     # def get(self, request):
     #     return HttpResponse('About Finch')
 
+class Finch_Create(CreateView):
+    model = Finch
+    fields = ['name', 'population', 'habitat']
+    template_name = 'finch_create.html'
+    success_url = '/finch/'
+
+class FinchDetail(DetailView):
+    model = Finch
+    template_name = 'finch_detail.html'
+
 class FinchList(TemplateView):
     template_name = "finchList.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["finches"] = Finch.objects.all()
+        name = self.request.GET.get('name')
+        if name != None:
+            context['finches'] = Finch.objects.filter(name__icontains=name)
+            context['header'] = f'Searching for {name}'
+        else:
+            context["finches"] = Finch.objects.all()
+            context['header'] = 'Our Finches'
         return context
