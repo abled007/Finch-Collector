de278809e1c199aebdfa9471918845b6ee273b39
abled007 <93559383+abled007@users.noreply.github.com>
2022-04-30 13:10:27 -0700

    Added FinchToy Model

--- main_app/models.py
+++ main_app/models.py
@@ -17,9 +17,16 @@ class Finch(models.Model):
     habitat = models.CharField(max_length=25, choices=HABITAT)
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    
+
     def __str__(self):
         return self.name
 
     class Meta:
         ordering = ['name']
+
+class FinchToy(models.Model):
+    name = models.CharField(max_length=100)
+    color = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
